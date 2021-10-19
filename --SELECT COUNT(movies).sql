6917580999130f9cc1b4c839d86df538289867aa
ppate304 <91405701+ppate304@users.noreply.github.com>
2021-10-19 13:39:19 -0500

    Add files via upload

---
+++ --SELECT COUNT(movies).sql
@@ -0,0 +1,66 @@
+--SELECT COUNT(last_name)
+--FROM actor
+--WHERE last_name LIKE 'Wehlburg';
+
+--SELECT COUNT(amount)
+--FROM payment
+--WHERE amount BETWEEN 3.99 AND 5.99; 
+
+--SELECT COUNT(film_id), film_id
+--FROM inventory
+--GROUP BY film_id
+--ORDER BY COUNT(film_id) DESC
+--LIMIT 1;
+
+--SELECT last_name
+--FROM customer
+--WHERE last_name LIKE 'William' ;
+
+--SELECT staff_id, COUNT(rental_id)
+--FROM payment
+--GROUP BY staff_id;
+
+--SELECT COUNT(district)
+--FROM address
+
+--SELECT COUNT(film_actor), film_id
+--From film_actor
+--GROUP BY film_id
+--ORDER BY COUNT(film_actor)
+--LIMIT 1;
+
+--SELECT COUNT(last_name)
+--FROM customer
+--WHERE last_name LIKE '%es'
+
+--SELECT COUNT(customer_id)
+--FROM payment 
+--GROUP BY amount
+--HAVING COUNT(customer_id) > 250
+--LIMIT 1;
+
+--SELECT COUNT(rating) 
+--FROM film
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
